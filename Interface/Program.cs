9b7b6a57fa48194f5e588028e00e80b6a56ae17d
Isak Wallin Färje <isakwfarje@outlook.com>
2025-10-21 21:27:32 +0200

    Add project files.

---
+++ Interface/Program.cs
@@ -0,0 +1,13 @@
+﻿using Backend;
+namespace Interface
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
