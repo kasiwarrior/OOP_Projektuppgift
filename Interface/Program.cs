c7a5818e96155230c6e91a03867ac42b3c138e63
Fredrik Domert Eriksson <fredrik.domert@gmail.com>
2025-10-28 20:01:29 +0100

    .

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -8,22 +8,36 @@ namespace Interface
         {
             WorkerRegistry workerRegistry = new WorkerRegistry();
 
-            Ant temp = new Ant(10238, "Ann-Sophie Gunnarson", WorkType.Ant, ShiftType.Night);
-            //workerRegistry.AddWorker(temp.GetId(), temp);
-            //temp = new Ant(2, "Isak2");
-            //workerRegistry.AddWorker(temp.GetId(), temp);
-            //temp = new Ant(3, "Isak3");
-            //workerRegistry.AddWorker(temp.GetId(), temp);
-            //temp = new Ant(4, "Isak4");
-            //workerRegistry.AddWorker(temp.GetId(), temp);
+            
 
-            workerRegistry.CreateBackup();
-
-            Console.WriteLine(temp);
-            //workerRegistry.LoadBackup();
-            //Console.WriteLine("Done");
-            //workerRegistry.CreateBackup();
-            //workerRegistry.LoadBackup();
+            Ant temp = new Ant(id: 1, "Myra1", workType: WorkType.Ant, ShiftType.Day, workShoes: true);
+            workerRegistry.AddWorker(temp.GetId(), temp);
+            temp = new Ant(id: 2, "Myra2", workType: WorkType.Ant, ShiftType.Day, workShoes: true);
+            workerRegistry.AddWorker(temp.GetId(), temp);
+            temp = new Ant(id: 3, "Myra3", workType: WorkType.Ant, ShiftType.Day, workShoes: true);
+            workerRegistry.AddWorker(temp.GetId(), temp);
+            
+            
+            workerRegistry.TestPrinter();
+            Console.WriteLine("Tryck för att skriva ut listan igen....");
+            Console.ReadKey();
+            workerRegistry.UpdateWorkerShift(1,ShiftType.Night);
+            workerRegistry.UpdateWorkerShift(2,ShiftType.Night);
+            workerRegistry.UpdateWorkerShift(3,ShiftType.Night);
+            
+            workerRegistry.UpdateWorkerName(1, "Fredrik Domert");
+            workerRegistry.UpdateWorkerName(2, "Pär Hedström");
+            workerRegistry.UpdateWorkerName(3, "Isak Wallin Färje");
+            
+            workerRegistry.UpdateWorkerShoes(1, false);
+            workerRegistry.UpdateWorkerShoes(2, false);
+            workerRegistry.UpdateWorkerShoes(3, false);
+            
+            Console.WriteLine("Skapa myra");
+            
+            //Console.WriteLine("Vilken myra vill du avliva?");
+            //int antPick = int.Parse(Console.ReadLine());
+            //workerRegistry.RemoveWorker(antPick);
             workerRegistry.TestPrinter();
         }
     }
